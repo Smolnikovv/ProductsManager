1ab533608a19a33487c8fd2daf9e0b4adbb95f04
Marcin Smolen <smolen.m@e80group.com>
2023-12-05 16:19:56 +0100

    files

---
+++ ProductsManager/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsManager.Services;
+
+namespace ProductsManager.Controllers
+{
+    [Route("api/product")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet("SKU")]
+        public ActionResult GetBySku([FromRoute]string SKU) 
+        {
+            var result = _productService.GetBySKU(SKU);
+
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ ProductsManager/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using ProductsManager.Model;
+using ProductsManager.Config;
+
+namespace ProductsManager.Services
+{
+    public interface IProductService
+    {
+        public List<ProductDto> GetBySKU(string SKU);
+    }
+    public class ProductService : IProductService
+    {
+        private readonly Mapper _mapper;
+        private readonly DatabaseContext _context;
+        public ProductService(Mapper mapper, DatabaseContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        public List<ProductDto> GetBySKU(string SKU)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
