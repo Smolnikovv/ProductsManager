217ed8e65769ef72a110e047b20760046a3dd102
Marcin Smolen <smolen.m@e80group.com>
2023-12-06 08:21:35 +0100

    data controller and service file

---
+++ ProductsManager/Services/DataService.cs
@@ -0,0 +1,24 @@
+﻿using ProductsManager.Config;
+
+namespace ProductsManager.Services
+{
+    public interface IDataService
+    {
+        public bool InsertData();
+    }
+    public class DataService : IDataService
+    {
+        private readonly Mapper _mapper;
+        private readonly DatabaseContext _databaseContext;
+        public DataService(Mapper mapper, DatabaseContext databaseContext)
+        {
+            _mapper = mapper;
+            _databaseContext = databaseContext;
+        }
+
+        public bool InsertData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
