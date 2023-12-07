ba82cc0b1e0fd4259452cb2abee3c09fc285980a
Marcin Smolen <smolen.m@e80group.com>
2023-12-07 16:37:35 +0100

    update

--- ProductsManager/Services/DataService.cs
+++ ProductsManager/Services/DataService.cs
@@ -1,4 +1,7 @@
-﻿using ProductsManager.Config;
+﻿using AutoMapper;
+using ProductsManager.Config;
+using ProductsManager.Entities;
+using ProductsManager.Model;
 
 namespace ProductsManager.Services
 {
@@ -8,9 +11,9 @@ namespace ProductsManager.Services
     }
     public class DataService : IDataService
     {
-        private readonly MappingProfile _mapper;
+        private readonly IMapper _mapper;
         private readonly DatabaseContext _databaseContext;
-        public DataService(MappingProfile mapper, DatabaseContext databaseContext)
+        public DataService(IMapper mapper, DatabaseContext databaseContext)
         {
             _mapper = mapper;
             _databaseContext = databaseContext;
@@ -18,7 +21,62 @@ namespace ProductsManager.Services
 
         public bool InsertData()
         {
-            throw new NotImplementedException();
+            #region Products
+            try
+            {
+
+            }
+            catch
+            {
+                return false;
+            }
+            #endregion
+            #region Inventory
+            try
+            {
+
+            }
+            catch
+            {
+                return false;
+            }
+            #endregion
+            #region Prices
+            try
+            {
+                var pricesMemory = GetFile("#");
+                var dto = PriceDto();
+
+                foreach(var item in dto)
+                {
+                    var price = _mapper.Map<Prices>(dto);
+                    _databaseContext.Prices.Add(price);
+                    _databaseContext.SaveChanges();
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            #endregion
+            return true;
+        }
+        private MemoryStream GetFile(string path)
+        {
+            var memory = new MemoryStream();
+            var net = new System.Net.WebClient();
+            var data = net.DownloadData(path);
+            var content = new System.IO.MemoryStream(data);
+            memory = content;
+            memory.Position = 0;
+            return memory;
+        }
+        private List<CreatePriceDto> PriceDto()
+        {
+            List<CreatePriceDto> dto = new List<CreatePriceDto>();
+
+
+            return dto;
         }
     }
 }
